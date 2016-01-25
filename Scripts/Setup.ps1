c9a00c1eb91ec8e18edca1962232d205019abfd5
Morgan Overman <Smorgan06@gmail.com>
2016-01-25 10:35:54 -0500

    Update Setup.ps1
    Fixed an issue in the Server module to tighten the code so that only Server copies run ServerPrep Script.

--- Scripts/Setup.ps1
+++ Scripts/Setup.ps1
@@ -28,7 +28,7 @@ REN "$env:windir\System32\runonce.exe" "runonce.exe.dis"}
 cd $default\Run
 #
 # Run Server Script and Module check
-if ($ServerPrepMod -eq "True"){
+if (($ServerPrepMod -eq "True") -and ($ServerMod -eq "True")){
 . .\Server.ps1; Server "ServerPrep"}
 #
 # Load Apps script and Run Setup Method
