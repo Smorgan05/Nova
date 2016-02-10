b18e139ebdc1891ffa9b5ea7116caf6bdeb6f800
Smorgan05 <Smorgan06@gmail.com>
2016-02-09 20:31:12 -0500

    Prototype 5
    Added Prototype 5 to Oem Branding

--- $OEM$/$$/Setup/Scripts/Run/Nova.ps1
+++ $OEM$/$$/Setup/Scripts/Run/Nova.ps1
@@ -10,7 +10,7 @@ cd $default
 
 if ($NovaMod -eq "True"){
 New-ItemProperty $OEMkey -Name Manufacturer -Value "Nova Edition" -Force | out-null
-New-ItemProperty $OEMkey -Name Model -Value "Nova v12.1" -Force | out-null
+New-ItemProperty $OEMkey -Name Model -Value "Nova v12.1 Prototype 5" -Force | out-null
 New-ItemProperty $OEMkey -Name Logo -Value "$home\Nova Pack\Themes\Nova.bmp" -Force | out-null
 
 # Start unpacking of Nova Pack
